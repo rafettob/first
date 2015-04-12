48225f8c49b5d051b045982e6a0e8d91816e06e8
Benjamin Rafetto <benjaminrafetto@Benjamins-iMac.local>
2015-04-12 12:41:23 -0400

    Added bootstrap support

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var config = require('./config')(process.argv[2] || 'uat');
+var session = require('express-session');
 
 //require('./router')(app);
 
@@ -12,41 +13,54 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').renderFile);
 
+app.use(session({
+  secret: process.env.EXPRESS_SECRET || '$#%!@#@@#SSDASAGDJDVV@@@@',
+  key: 'sid'
+}));
 
-  app.get('/', function(req, res) {
-    res.render('index.ejs', {
-      title: "Rentr."
-    });
+app.use(function(req, res, next) {
+  //console.dir(req.session);
+  next();
+});
+
+app.get('/', function(req, res) {
+  res.render('index.ejs', {
+    title: "Rentr."
   });
-  
-  app.get('/find', function(req, res) {
-    res.render('find.ejs', {
-      title: "Rentr - Find Your Apartment."
-    });
+});
+
+app.post('/login', function(req, res) {
+
+})
+
+app.get('/find', function(req, res) {
+  res.render('find.ejs', {
+    title: "Rentr - Find Your Apartment."
   });
+});
 
-  app.get('/upload', function(req, res) {
-    res.render('upload.ejs', {
-      title: "Rentr - Post Your Apartment."
-    });
+app.get('/upload', function(req, res) {
+  res.render('upload.ejs', {
+    title: "Rentr - Post Your Apartment."
   });
+});
 
-  app.get('/about', function(req, res) {
-    res.render('about.ejs', {
-      title: "About Rentr."
-    });
+app.get('/about', function(req, res) {
+  res.render('about.ejs', {
+    title: "About Rentr."
   });
+});
 
-  app.get('/signup', function(req, res) {
-    res.render('signup.ejs', {
-      title: "The index2 page."
-    });
+app.get('/signup', function(req, res) {
+  res.render('signup.ejs', {
+    title: "The index2 page."
   });
+});
 
-  app.use('/public', express.static(__dirname + '/public'));
-  app.use('/bower_components', express.static(__dirname + '/bower_components'));
-  app.use('/scripts', express.static(__dirname + '/scripts'));
-
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/', express.static(__dirname + '/bower_components/bootstrap/dist'));
 
   // app.get('/admin', function(req, res) {
   //   res.render('admin.ejs', {
@@ -54,19 +68,6 @@ app.engine('ejs', require('ejs').renderFile);
   //   });
   // });
 
-//app.use(express.static('views'));
-
-// app.set('views', __dirname + '/views');
-
-
 // app.get('/login', function(req, res) {
 // 	res.end('This will someday be a login page.');
-// });
-
-// app.get('/find', function(req, res) {
-// 	res.end("Here you'll be able to find an apartment someday.");
-// });
-
-// app.get('/add', function(req, res) {
-// 	res.end("Coming soon, you'll be able to upload your own apartment!");
 // });
